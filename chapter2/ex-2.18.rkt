dcc11fc56fd3c1a08c02cfeb1e2b8b9aa9a83066
Frovu <sfrovis@gmail.com>
2021-01-04 13:44:33 +0300

    add: 2.18 v2

--- chapter2/ex-2.18.rkt
+++ chapter2/ex-2.18.rkt
@@ -4,4 +4,11 @@
 	(if (null? l) null
 		(append (reverse (cdr l)) (list (car l)))))
 
+(define (reverse-it l)
+	(define (iter res rem)
+		(if (null? rem) res
+			(iter (cons (car rem) res) (cdr rem))))
+	(iter '() l))
+
 (reverse (list 1 4 9 16 25))
+(reverse-it (list 1 4 9 16 25))
